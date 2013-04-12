9bc86c0bfe063a486e4b8b9cee2135ff68298f17
U-ck-laptop\ck <ck@ck-laptop.(none)>
2013-04-12 17:02:20 +0900

    Minor bugfix Version 0.4.2

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,26 +1,28 @@
 var $panelLeft = $('.panel.left');
-var leftPanelPixels = $panelLeft.outerWidth(true) + $panelLeft.offset().left;
+if($panelLeft[0]) {
+  var leftPanelPixels = $panelLeft.outerWidth(true) + $panelLeft.offset().left;
 
-$('body').append($('<div>').attr('id', 'zoom-imgur-modal').
-  css({left: leftPanelPixels, position: 'absolute', 'z-index': 9999}).
-  append($('<img>').css({border: '5px solid black'})).hide());
+  $('body').append($('<div>').attr('id', 'zoom-imgur-modal').
+    css({left: leftPanelPixels, position: 'absolute', 'z-index': 9999}).
+    append($('<img>').css({border: '5px solid black'})).hide());
 
-var $modal = $('#zoom-imgur-modal');
-var displayModal = function(e) {
-  var width = screen.availWidth - leftPanelPixels - 40;
-  $modal.css('top', window.scrollY + 50).
-    find('img').attr({src: e.target.src.replace('b.', '.'), width: width});
-  $modal.show();
-};
+  var $modal = $('#zoom-imgur-modal');
+  var displayModal = function(e) {
+    var width = screen.availWidth - leftPanelPixels - 40;
+    $modal.css('top', window.scrollY + 50).
+      find('img').attr({src: e.target.src.replace('b.', '.'), width: width});
+    $modal.show();
+  };
 
-var loadedImagesCount = 0;
-var load = function(selector) {
-  $(selector).map(function(){return this.src.replace('b.', '.')}).
-    each(function(){loadedImagesCount++; var img = new Image; img.src = this});
-};
+  var loadedImagesCount = 0;
+  var load = function(selector) {
+    $(selector).map(function(){return this.src.replace('b.', '.')}).
+      each(function(){loadedImagesCount++; var img = new Image; img.src = this});
+  };
 
-$('#imagelist').on({mouseover: displayModal, mouseout: function(){$modal.hide()}}, '.post img');
-$(window).on({
-  load: function(){load('.posts img')},
-  scroll: function(){if($('.posts img').size() > loadedImagesCount) load('.posts:last img')}
-});
+  $('#imagelist').on({mouseover: displayModal, mouseout: function(){$modal.hide()}}, '.post img');
+  $(window).on({
+    load: function(){load('.posts img')},
+    scroll: function(){if($('.posts img').size() > loadedImagesCount) load('.posts:last img')}
+  });
+}
